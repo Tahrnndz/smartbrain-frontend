c10826f131ba9298b3e5b6e511a08fbdf65a136a
Trey Hernandez <Tahrnndz@gmail.com>
2021-05-31 17:32:40 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ loadUser = (data) => {
 
     onButtonSubmit = () => {
       this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://guarded-thicket-09990.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -90,7 +90,7 @@ loadUser = (data) => {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://guarded-thicket-09990.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://guarded-thicket-09990.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
